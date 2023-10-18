b2f90a079d3cefa5ea4cd016b13b6be25263c903
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-19 01:14:09 +0530

    ⚡️ feat: useReducer introduced

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import DateCounter from './DateCounter';
 
+/**
+ * ? what is useReduce hook
+ * it is a more advanced and more complex way of managing state
+ * instead of the useState hook
+ *
+ */
+
 function App() {
   return (
     <>
--- src/DateCounter.jsx
+++ src/DateCounter.jsx
@@ -1,57 +1,89 @@
-import { useState } from "react";
+import { useReducer, useState } from 'react';
+
+const reducer = function (currState, action) {
+  console.log(currState, action);
+
+  if (action.type === 'increment') return currState + 1;
+  if (action.type === 'decrement') return currState - 1;
+  if (action.type === 'setCount') return action.payload;
+};
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0);
+
   const [step, setStep] = useState(1);
 
   // This mutates the date object.
-  const date = new Date("june 21 2027");
+  const date = new Date('june 20 2027');
   date.setDate(date.getDate() + count);
 
-  const dec = function () {
+  const decrement = function () {
+    /** ACTION OF USE REDUCER FUNCTION
+     * ! let's use action of reducer function
+     * * this object we passed in is called as action when we work with reducer function
+     * * this object we made could have any shape that we wanted
+     * ! but it is standard to just have type property and payload property
+     * ! this is the standard we will always see when using the useReducer hook
+     */
+    dispatch({ type: 'decrement' });
+
+    // dispatch(-1);
+
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    // setCount((count) => count - step);
   };
 
-  const inc = function () {
+  const increment = function () {
+    dispatch({ type: 'increment' });
+
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    // setCount((count) => count + step);
   };
 
+  // user input onChange function
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    dispatch({ type: 'setCount', payload: Number(e.target.value) });
+
+    // setCount(Number(e.target.value));
   };
 
+  // slider onChange function
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    dispatch({ type: 'setCount', payload: Number(e.target.value) });
+    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
-    setCount(0);
+    // setCount(0);
     setStep(1);
   };
 
   return (
-    <div className="counter">
+    <div className='counter'>
+      {/* range meter */}
       <div>
         <input
-          type="range"
-          min="0"
-          max="10"
+          type='range'
+          min='0'
+          max='10'
           value={step}
           onChange={defineStep}
         />
         <span>{step}</span>
       </div>
 
+      {/* decrement, value, increment */}
       <div>
-        <button onClick={dec}>-</button>
+        <button onClick={decrement}>-</button>
         <input value={count} onChange={defineCount} />
-        <button onClick={inc}>+</button>
+        <button onClick={increment}>+</button>
       </div>
 
+      {/* readable date format */}
       <p>{date.toDateString()}</p>
 
+      {/* reset button */}
       <div>
         <button onClick={reset}>Reset</button>
       </div>
