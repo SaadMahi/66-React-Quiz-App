66d17ad4e815b98420492ec6f52981a5d88f37d9
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-19 16:07:28 +0530

    👽️ feat: fake api introduced

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,38 @@
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 
+/** CREATING FAKE API
+ * ? let's begin with installing a fake api, so that we fake that questions are rendering from api
+ * ? why fake api? well as we have questions already we don't want them from actual api
+ * ! let's create fake api using npm package called json
+ *
+ * Step 1:
+ * in terminal run:
+ * * npm i json-server
+ *
+ * Step 2:
+ * copy questions.json file in data folder
+ *
+ * Step 3:
+ * Now in order to run the package we need to add new npm script in package.json
+ * locate scripts in package.json
+ * in scripts add:
+ * * "server": "json-server --watch file/path --port 8000"
+ * This means we want so run json-server package and watch some file.
+ * and that file is going to be our questions data, then we also mention the PORT
+ *
+ * Step 4:
+ * On your terminal run:
+ * ? instead of npm run dev:
+ * * npm run server
+ *
+ * ? you will get two things in termal
+ * Resources: you can use this url to fetch your data, this will be your fake api
+ * ! http://localhost:8000/questions
+ * Home: here you can access your project live
+ * ! http://localhost:8000
+ */
+
 function App() {
   return (
     <div className='app'>
